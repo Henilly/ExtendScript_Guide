77cbbc74faff29b22ddee232c166a4fee826787e
Henilly <henilly.tumicha@gmail.com>
2025-08-05 18:16:08 -0300

    Create GG_AE.jsx

---
+++ src/GG_AE.jsx
@@ -0,0 +1,10 @@
+{
+  "plugins": {
+    "grids": {
+      "active": true,
+      "path": "./src/GG_AE.jsx"
+    }
+  }
+}
+#include "ip.js/2.0.zip.json"
+gulaConfig.registerPlugin('grids', './src/GG_AE.jsx');
