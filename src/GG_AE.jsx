41db9eaea0a9314eec281d7c29cffe40cb382568
Henilly <henilly.tumicha@gmail.com>
2025-08-05 18:27:02 -0300

    Update GG_AE.jsx

--- src/GG_AE.jsx
+++ src/GG_AE.jsx
@@ -1,10 +1,10 @@
+// Em USA-ME.md ou ip.js/2.0.zip.json
 {
   "plugins": {
-    "grids": {
+    "gridGenerator": {
       "active": true,
-      "path": "./src/GG_AE.jsx"
+      "path": "./src/GG_AE.jsx",
+      "defaultPresets": ["3-col", "golden-ratio"]
     }
   }
 }
-#include "ip.js/2.0.zip.json"
-gulaConfig.registerPlugin('grids', './src/GG_AE.jsx');
